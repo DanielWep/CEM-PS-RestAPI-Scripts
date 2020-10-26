3e56ebf3a10f27f3541170233561c63ef362caf1
Daniel Weppeler <daniel.weppeler@braincon.de>
2020-10-26 14:20:14 +0100

    update CEM-Enable-APNS-HTTP2.ps1

--- CEM-Enable-APNS-HTTP2.ps1
+++ CEM-Enable-APNS-HTTP2.ps1
@@ -10,18 +10,12 @@ $headers = @{}
 $base = "https://" + $hostname + ":4443/xenmobile/api/v1"
 $password = Read-Host "Enter Password" -AsSecureString | ConvertFrom-SecureString | Out-File "$env:TEMP\login.txt"
 $password = (Get-Content "$env:TEMP\login.txt") | ConvertTo-SecureString
-if (!$username) {
-    Write-Host "Error: Username is missing. Please try again!" -ForegroundColor Red
-    $username = Read-Host "Enter Username"
-} else {
-    try {
+try {
     $loginbody = '{"login":"' + $username + '","password":"' + [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($password)))) +'"}'
     $session = Invoke-RestMethod -Headers $headers -Uri "$base/authentication/login" -Method Post -Body $loginbody -ContentType 'application/json'
     $auth_token = $session.auth_token
     $headers.Add("auth_token", "$auth_token")
     $restapicmd = Invoke-RestMethod -Uri "$base/serverproperties" -Body '{"name":"apple.apns.http2","value":"true","displayName":"apple.apns.http2","description":"Enable HTTP/2 for APNS"}' -ContentType application/json -Headers $headers -Method Post
-    $restapicmd.status
-
     if ($restapicmd.status -eq "0") {
         Write-Host "Status: APNS HTTP2 Server Property successfully installed." -ForegroundColor Green 
         Write-Host "Info: No restart is required for the Cloud Edition, but a restart is required for the OnPrem Edition." -ForegroundColor Green
@@ -33,4 +27,3 @@ if (!$username) {
     Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
     }
-}
